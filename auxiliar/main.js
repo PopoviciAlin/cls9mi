617558a908d010382510e01356a9444198a2ab47
Popovici Alin <popovicialincatalin@gmail.com>
2023-06-30 18:25:29 +0300

    1

---
+++ auxiliar/main.js
@@ -0,0 +1,36 @@
+// Function to toggle between dark and light themes
+function toggleTheme() {
+    const currentTheme = localStorage.getItem('theme');
+  
+    if (currentTheme === 'light') {
+      applyTheme('dark');
+    } else {
+      applyTheme('light');
+    }
+  }
+  
+  // Function to apply the selected theme
+  function applyTheme(theme) {
+    const themeLink = document.getElementById('theme');
+    themeLink.href = `auxiliar/${theme}.css`;
+    localStorage.setItem('theme', theme);
+  
+    // Add the custom-transition class after a small delay
+    setTimeout(function() {
+      const titleElement = document.getElementById('title');
+      titleElement.classList.add('custom-transition');
+    }, 1000);
+  }
+  
+  // Function to load the user preference from local storage
+  function loadTheme() {
+    const theme = localStorage.getItem('theme');
+    if (theme) {
+      applyTheme(theme);
+    }
+  }
+  
+  // Load the user preference on page load
+  loadTheme();
+  
+  
