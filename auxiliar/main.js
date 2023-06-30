77e8885a7d81968921a4ad4a2d8dacf8289575bb
Popovici Alin <popovicialincatalin@gmail.com>
2023-06-30 19:38:42 +0300

    1

--- auxiliar/main.js
+++ auxiliar/main.js
@@ -1,38 +1,41 @@
-// Function to toggle between dark and light themes
 function toggleTheme() {
     const currentTheme = localStorage.getItem('theme');
-  
+
     if (currentTheme === 'light') {
-      applyTheme('dark');
+        applyTheme('dark');
     } else {
-      applyTheme('light');
+        applyTheme('light');
     }
-  }
-  
-  // Function to apply the selected theme
-  function applyTheme(theme) {
+}
+
+// Function to apply the selected theme
+function applyTheme(theme) {
     const themeLink = document.getElementById('theme');
     themeLink.href = `auxiliar/${theme}.css`;
     localStorage.setItem('theme', theme);
-  
+
     // Add the custom-transition class after a small delay
     setTimeout(function() {
-      const titleElement = document.getElementById('title');
-      titleElement.classList.add('custom-transition');
+        const titleElement = document.getElementById('titlu');
+        titleElement.classList.add('custom-transition');
+
+        // Update the text within the paragraph based on the selected theme
+        const themeText = document.getElementById('theme-text');
+        if (theme === 'light') {
+            themeText.textContent = 'Întunecată';
+        } else {
+            themeText.textContent = 'Luminoasă';
+        }
     }, 100);
-  }
-  
-  // Function to load the user preference from local storage
-  function loadTheme() {
-    const theme = localStorage.getItem('theme');
-    if (theme) {
-      applyTheme(theme);
+}
+
+// Function to load the user preference from local storage
+function loadTheme() {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        applyTheme(savedTheme);
     }
-  }
-  
-  // Load the user preference on page load
-  loadTheme();
-  
-  // Add event listener to the theme toggle button
-  document.getElementById('theme-toggle').addEventListener('click', toggleTheme);
-  
+}
+
+// Load the theme on initial page load
+loadTheme();
